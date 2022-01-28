38d240015569e9c1f7f1a63ffc12e7fb2097c736
Islam Kortam <eng.islam.kortam@gmail.com>
2022-01-28 21:49:18 +0200

    Adding PlayerStatus Enum

---
+++ ClientSideApp/GameBoard/src/controllerPackage/PlayerStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllerPackage;
+
+/**
+ *
+ * @author imkor
+ */
+public enum PlayerStatus {
+    OFFLINE,
+    ONLINE,
+    IN_MULTIPLAYER_GAME,
+    IN_SINGLE_PLAYER_GAME
+}
